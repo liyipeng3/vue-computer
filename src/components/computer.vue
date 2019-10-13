7169e066c35543b7568ecc9eba28bd6d9d414ec0
李奕鹏 <17895987@qq.com>
2019-10-13 17:48:48 +0800

    Update computer.vue

--- src/components/computer.vue
+++ src/components/computer.vue
@@ -4,6 +4,7 @@
         <div class="computer">
             <el-button @click="output(num)" v-for="num in nums" >{{num}}</el-button>
         </div>
+      <el-button @click="now++"></el-button>
     </div>
 </template>
 
@@ -13,14 +14,20 @@
         data() {
             return {
                 nums: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-                result: ''
+                current: '',
+                now:0
             };
         },
         methods: {
           output(num) {
             this.result += num;
             },
-        }
+        },
+      computed:{
+          result:function() {
+            return this.now*50;
+          }
+      }
     };
 </script>
 
