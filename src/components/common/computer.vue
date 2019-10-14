784b1b216a610a55dc9c285ea84d0c9ac4430920
1173710104 <765816432@qq.com>
2019-10-14 16:06:26 +0800

    添加computeResult函数

--- src/components/common/computer.vue
+++ src/components/common/computer.vue
@@ -84,8 +84,134 @@
 
             },
             computeResult() {
-                this.result = 'Output:' + this.current;
-                this.inputting = false;
+                /*-----------中缀表达式转后缀表达式---------*/
+                var end = []
+                var i = 0, j = 0
+                var stack = [];
+                while (i < this.current.length) {
+                    if (this.current[i] <= '9' && this.current[i] >= '0') {
+                        end[j] = this.current[i];
+                        //alert(end[j]+"进后缀表达式")
+                        i++;
+                        j++;
+                        if (this.current[i] >= '9' || this.current[i] <= '0' || i === this.current.length)   //若数字后一位为操作符则添加空行以便后续计算
+                        {
+                            end[j] = ' ';
+                            j++;
+                        }
+                    } else if (this.current[i] === '(')   //遇到'('则直接入栈
+                    {
+                        stack.push(this.current[i]);
+                        i++;
+                        //alert("(入栈")
+                    } else if (this.current[i] === ')')   //遇到')'则弹出栈直到遇到'('，'('也弹出但不添加到后缀表达式中
+                    {
+                        while (stack[stack.length - 1] !== '(') {
+                            end[j] = stack.pop();
+                            //alert(end[j]+"出栈进后缀表达式")
+                            j++;
+                            end[j] = ' ';
+                            j++;
+                        }
+                        stack.pop();   //弹出'('
+                        //alert("(出栈")
+                        i++;
+                    } else if (this.current[i] === '+' || this.current[i] === '-') {      //遇到加减时
+                        if (stack[stack.length - 1] === '*' || stack[stack.length - 1] === '/' || stack[stack.length - 1] === '+'
+                            || stack[stack.length - 1] === '-')      //若栈顶元素为*，/，+，-，则栈顶元素出栈，新元素进栈
+                        {
+                            end[j] = stack.pop();
+                            //alert(end[j]+"出栈进后缀表达式")
+                            j++;
+                            end[j] = ' ';
+                            j++;
+                            stack.push(this.current[i]);
+                            //alert(this.current[i]+"进栈")
+                            i++;
+                        } else if (stack.length === 0 || stack[stack.length - 1] === '(')   //若栈为空，则入栈
+                        {
+                            stack.push(this.current[i]);
+                            //alert(this.current[i]+"进栈")
+                            i++;
+                        }
+                    } else if (this.current[i] === '*' || this.current[i] === '/')   //遇到乘除时
+                    {
+                        if (stack[stack.length - 1] === '+' || stack[stack.length - 1] === '-')  //若栈顶为+，-，则新元素进栈
+                        {
+                            stack.push(this.current[i]);
+                            i++;
+                        } else if (stack[stack.length - 1] === '*' || stack[stack.length - 1] === '/')   //若栈顶为*，/则栈顶元素出栈到后缀表达式，新元素进栈
+                        {
+                            end[j] = stack.pop();
+                            j++;
+                            end[j] = ' ';
+                            j++;
+                            stack.push(this.current[i]);
+                            i++;
+                        } else if (stack[stack.length - 1] === '(' || stack.length === 0)  //若栈顶为（或为空栈，则入栈
+                        {
+                            stack.push(this.current[i]);
+                            i++;
+                        }
+                    }
+                }
+                while (stack.length !== 0) //弹出栈里所有操作符
+                {
+                    end[j] = stack.pop();
+                    //alert(end[j]+"出栈进后缀表达式")
+                    j++;
+                    end[j] = ' ';
+                    j++;
+                }
+                end.pop();
+
+                /*---------计算后缀表达式的值------------*/
+                var numstack = [];
+                var sum = 0, i = 0;
+                var num1,num2;
+                while (i <= end.length) {
+                    //alert(end[i])
+                    if(end[i]>='0' && end[i]<='9'){     //若该元素为数字，
+                        sum = sum*10+(parseInt(end[i]));
+                        if(end[i+1]===' '){           //若下个元素为空格，则与前面的数字进行运算后入栈，否则存储该数字
+                            numstack.push(parseFloat(sum));
+                            //alert(numstack[numstack.length-1]+"进栈")
+                            i+=2;
+                            sum=0;
+                        }
+                        else
+                            i++;
+                    }else if(end[i]==='+') {
+                        num2=numstack.pop();
+                        num1=numstack.pop();
+                        numstack.push(num1+num2);
+                        //alert(num1+","+num2+"出栈计算，"+numstack[numstack.length-1]+"进栈")
+                        i+=2;
+                    }else if(end[i]==='-')
+                    {
+                        num2=numstack.pop();
+                        num1=numstack.pop();
+                        numstack.push(num1-num2);
+                        //alert(num1+","+num2+"出栈计算，"+numstack[numstack.length-1]+"进栈")
+                        i+=2;
+                    } else if(end[i]==='*') {
+                        num2=numstack.pop();
+                        num1=numstack.pop();
+                        numstack.push(num1*num2);
+                        //alert(num1+","+num2+"出栈计算，"+numstack[numstack.length-1]+"进栈")
+                        i+=2;
+                    }
+                    else if(end[i]==='/')
+                    {
+                        num2=numstack.pop();
+                        num1=numstack.pop();
+                        numstack.push(num1/num2);
+                        //alert(num1+","+num2+"出栈计算，"+numstack[numstack.length-1]+"进栈")
+                        i+=2;
+                    }
+                }
+
+                this.result = numstack.pop();
             },
             clear() {
                 this.result = '';
